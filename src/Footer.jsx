a0c6ec3960da9ce6532914041a0f8fedd63cd66f
devioand talha <talhatjaved5@gmail.com>
2025-08-14 21:06:24 +0500

    added change

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <footer className="bg-gradient-to-tr from-green-400 via-green-500 to-green-600 text-white rounded-2xl mb-4 ml-2.5 mr-2.5 py-8 mt-10 text-center relative">
+      <footer className="bg-gradient-to-tr grid justify-center items-center from-green-400 via-green-500 to-green-600 text-white rounded-2xl mb-4 ml-2.5 mr-2.5 py-8 mt-10 text-center relative">
         
         <div className="flex flex-col md:flex-row flex-wrap items-center justify-center gap-2 mb-4">
           <span className="inline-flex items-center gap-2  font-semibold text-lg text-white">
@@ -18,7 +18,7 @@ const Footer = () => {
           <p className="text-green-400 font-normal text-lg bg-white ml-2 mr-2 rounded-2xl p-3">
             Developed by <span className="font-semibold text-green-600">Muhammad Talha Javed</span> &mdash; Passionate Frontend Developer
           </p>
-          <p className="text-gray-500 text-base mt-1  bg-white ml-2 mr-2 rounded-2xl p-3">
+          <p className="text-gray-500 text-base mt-1   bg-white ml-2 mr-2 rounded-2xl p-3">
             This site is a tribute to Pakistan's Independence Day, celebrated every year on <span className="text-green-400 font-bold">14th August</span>.
             <br />
             Let's remember the sacrifices of our ancestors and cherish the freedom of our beloved homeland. Pakistan Zindabad! 🇵🇰
